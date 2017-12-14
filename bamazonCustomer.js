5e745c14296a83e0496a8f092a57a5a1f1c0b0d8
Michael Balagia <mbalagia@gmail.com>
2017-12-14 14:49:25 -0600

    added functionality to remove quantity of consumers desired product

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,7 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 var wantedID;
 var currStock;
+var wantedAmt;
 
 
 
@@ -19,44 +20,101 @@ var connection = mysql.createConnection({
 
 
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Welcome to Bamazon, a service similar to, but for legal reasons, distinct from Amazon.")
-  displayItems();
+    if (err) throw err;
+    console.log("Welcome to Bamazon, a service similar to, but for legal reasons, distinct from Amazon.")
+    displayItems();
 });
 
 
-function displayItems(){
-	console.log("Here are the items we have in stock: \n");
-	var myQuery="select * from bamazon.products where 1 and stock_quantity>0"
-	connection.query(myQuery, function(err,res){
-		if (err) throw err;
-		for (var i=0; i< res.length; i++){
-			console.log(
-				"Item ID: "+ res[i].item_id +
-				" || Name: " + res[i].product_name +
-				" || Price: $" + res[i].price
-				);
-		}
-		// custIDPrompt();
-	});
+function displayItems() {
+    console.log("Here are the items we have in stock: \n");
+    var myQuery = "select * from bamazon.products where 1 and stock_quantity>0"
+    connection.query(myQuery, function(err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log(
+                "Item ID: " + res[i].item_id +
+                " || Name: " + res[i].product_name +
+                " || Price: $" + res[i].price
+            );
+        }
+        custIDPrompt();
+    });
 }
 
-function custIDPrompt() {
-  inquirer
-    .prompt({
-      name: "prod_id",
-      type: "input",
-      message: "What is the ID of the product you'd like to buy?",
-    })
-    .then(function(answer) {
-    	// answer.prod_id
-    	
+//run sql query when they give us id to store quantity as a variable
+// then use the second inq prompt answer to compare against that num
+// then if or case such that if false, return insuff quant
 
-    	// store id as a variable, then new inq promt asking how many
-    	// then run sql query to see if we have that many
-//----------------
-    	//or run sql query when they give us id to store quantity as a variable
-    	// then use the second inq prompt answer to compare against that num
-    	// then if or case such that if false, return insuff quant
-    });
+function custIDPrompt() {
+    inquirer
+        .prompt({
+            name: "prod_id",
+            type: "input",
+            message: "What is the ID of the product you'd like to buy?",
+            validate: function(value) {
+                if (isNaN(value) === false && value > 0) {
+                    return true;
+                }
+                return false;
+            }
+        })
+        .then(function(answer) {
+                wantedID = answer.prod_id;
+                console.log(wantedID);
+                var myQuery = "select * from bamazon.products where 1 and item_id=?";
+                connection.query(myQuery, [wantedID], function(err, res) {
+                    if (err) throw err;
+                    currStock = res[0].stock_quantity;
+                    howMany();
+                })
+            }
+
+        );
+}
+
+function howMany() {
+    inquirer
+        .prompt({
+            name: "amount_wanted",
+            type: "input",
+            message: "How many would you like?",
+            validate: function(value) {
+                if (isNaN(value) === false && value > 0) {
+                    return true;
+                }
+                return false;
+            }
+        })
+        .then(function(answer) {
+            wantedAmt = answer.amount_wanted;
+            if (wantedAmt > currStock) {
+                console.log(" ¯\\_(ツ)_/¯ Oops! We only have " + currStock + "! ¯\\_(ツ)_/¯ ");
+                howMany();
+            } else {
+                removeStock();
+            }
+
+
+        })
+}
+
+
+function removeStock() {
+    console.log("sellin stuff, man!");
+    var newAmt = (currStock - wantedAmt);
+    var myQuery = connection.query("UPDATE bamazon.products SET ? WHERE ?", [{
+                stock_quantity: newAmt
+            },
+            {
+                item_id: wantedID
+            }
+        ],
+        function(err, res) {
+            if (err) throw err;
+            console.log(res.affectedRows + " products updated!\n");
+            displayItems();
+        }
+    );
+    console.log(myQuery.sql);
 }
